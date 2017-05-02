7378e863b99c5256ad22a4e20563425299717ce9
doria <doria@164.81.236.248>
2017-05-02 09:57:41 +0200

    Fin TP2

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -3,6 +3,8 @@ package fr.unilim.iut.spaceinvaders;
     import static org.junit.Assert.assertEquals;
     import org.junit.Test;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
     public class SpaceInvadersTest {
 	
 	   @Test
@@ -19,6 +21,29 @@ package fr.unilim.iut.spaceinvaders;
 		    "...............\n" + 
 		    "...............\n" + 
 		    "...............\n" , spaceinvaders.toString());
-	        }
-
-       }
+	   }
+	   
+	   @Test
+		public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			".......V.......\n" , spaceinvaders.toString());
+		}
+	   
+	   @Test(expected = HorsEspaceJeuException.class)
+		public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+			
+		}
+}
