028be99cbe61d8d92e8abbc7fc8a24dd6fbcd5b7
dorian <dorian@dorian>
2017-05-22 09:55:51 +0200

    Ajout Sprite

---
+++ spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Sprite.java
@@ -0,0 +1,62 @@
+package fr.unilim.iut.spaceinvaders;
+
+public abstract class Sprite {
+
+	protected Position origine;
+	protected Dimension dimension;
+	protected int vitesse;
+
+	public Sprite(Position origine, Dimension dimension, int vitesse) {
+		super();
+		this.origine = origine;
+		this.dimension = dimension;
+		this.vitesse = vitesse;
+	}
+
+
+	public boolean occupeLaPosition(int x, int y) {
+		return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
+	}
+
+	public boolean estOrdonneeCouverte(int y) {
+		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
+	}
+
+	public boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
+	}
+
+	public int ordonneeLaPlusBasse() {
+		return this.origine.ordonnee() - this.dimension.hauteur() + 1;
+	}
+
+	public int ordonneeLaPlusHaute() {
+		return this.origine.ordonnee();
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.origine.abscisse() + this.dimension.longueur() - 1;
+	}
+
+	public int abscisseLaPlusAGauche() {
+		return this.origine.abscisse();
+	}
+
+	public void seDeplacerVersLaDroite() {
+		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
+	}
+
+	public void seDeplacerVersLaGauche() {
+		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
+	}
+
+	public void positionner(int x, int y) {
+		this.origine.changerAbscisse(x);
+		this.origine.changerOrdonnee(y);
+	}
+
+	public int longueur() {
+		return this.dimension.longueur();
+	}
+
+}
