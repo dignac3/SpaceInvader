4700135210e0ea1a2045d8dfb092bac1edbe82a4
dorian <dorian@dorian>
2017-05-22 09:37:21 +0200

    Ajout classe Sprite

--- spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,68 +1,8 @@
 package fr.unilim.iut.spaceinvaders;
 
-public class Vaisseau {
-
-	private Position origine;
-	private Dimension dimension;
-	private int vitesse;
-
-	public Vaisseau(int longueur, int hauteur) {
-		this(longueur, hauteur, 0, 0);
-	}
-
-	public Vaisseau(int longueur, int hauteur, int x, int y) {
-		this(new Dimension(longueur, hauteur), new Position(x, y), 1);
-	}
+public class Vaisseau extends Sprite {
 
 	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-		this.dimension = dimension;
-		this.origine = positionOrigine;
-		this.vitesse = vitesse;
+		super(positionOrigine, dimension, vitesse);
 	}
-
-	public boolean occupeLaPosition(int x, int y) {
-		return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
-	}
-
-	public boolean estOrdonneeCouverte(int y) {
-		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-	}
-
-	public boolean estAbscisseCouverte(int x) {
-		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-	}
-
-	public int ordonneeLaPlusBasse() {
-		return this.origine.ordonnee() - this.dimension.hauteur() + 1;
-	}
-
-	public int ordonneeLaPlusHaute() {
-		return this.origine.ordonnee();
-	}
-
-	public int abscisseLaPlusADroite() {
-		return this.origine.abscisse() + this.dimension.longueur() - 1;
-	}
-
-	public int abscisseLaPlusAGauche() {
-		return this.origine.abscisse();
-	}
-
-	public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	}
-
-	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	}
-
-	public void positionner(int x, int y) {
-		this.origine.changerAbscisse(x);
-		this.origine.changerOrdonnee(y);
-	}
-
-	public int longueur() {
-		return this.dimension.longueur();
-	}
-
 }
